05953e31ef79dfcc99ba88eb867c895e2786ce5f
Andrea Bender <andreab.1726@gmail.com>
2019-12-09 14:30:22 -0700

    10 complete

--- recursion-drills.js
+++ recursion-drills.js
@@ -146,6 +146,23 @@ const wayOut = function (mySmallMaze, y = 0, x = 0) {
 };
 console.log(wayOut(mySmallMaze));
 
+//Anagrams
+function findAnagrams(str) {
+    if (str.length === 1) return [str]
+
+    let all = []
+    for (let i = 0; i < str.length; i++) {
+        const currentLetter = str[i]
+        const remainingLetters = str.slice(0, i) + str.slice(i + 1)
+        const permsOfRemainingLetters = findPerms(remainingLetters)
+
+        permsOfRemainingLetters.forEach(subPerm => {
+            all.push(currentLetter + subPerm)
+        })
+    } return all
+}
+
+findAnagrams('east')
 
 //Binary Representation
 //print binary representation of a given number assume all numbers are positive
